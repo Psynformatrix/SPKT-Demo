dfa9b494a39713becefb1e5dd5e2a716794b20e8
Psynformatrix <sedlmeier.robert@web.de>
2023-04-24 09:32:09 +0200

    Add all

---
+++ nftdc/src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-white text-black">
+      <q-toolbar>
+        <q-avatar size="100px" class="q-ml-md">
+          <img src="../assets/DekaBank_Deutsche_Girozentrale_201x_logo.svg" width="10%">
+        </q-avatar>
+        <q-toolbar-title class="text-weight-bold q-ml-md">
+        NFT-Donation-Club
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+
+export default ({
+  name: 'MainLayout',
+
+})
+</script>
---
+++ nftdc/src/pages/IndexPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="flex flex-center bg-brown-2">
+    <q-card dark bordered class="inputcard bg-white my-card">
+      <q-card-section>
+        <div class="text-h6 text-black q-ma-md q-mb-xl">POAP Anmeldeformular</div>
+        <q-input outlined class="bg-grey-4 q-ma-md q-mb-lg" v-model="text" label="Name" />
+        <q-input outlined class="bg-grey-4 q-ma-md q-mb-lg" v-model="text" label="E-Mail-Adresse" />
+        <q-input outlined class="bg-grey-4 q-ma-md q-mb-lg" v-model="text" label="Sparkasse" />
+      </q-card-section>
+      <q-card-section>
+        <div>
+          <q-checkbox class="text-black" v-model="right" label="Hiermit stimme ich der Verarbeitung meiner Daten zu. Bla bla bla..." />
+        </div>
+      </q-card-section>
+      <q-card-section class="row flex-center">
+        <q-btn unelevated class="q-ma-md" color="red-14" label="Eingabe bestätigen" />
+        <q-btn unelevated class="q-ma-md" color="red-14" label="Pro-Button mit QR-Code" />
+      </q-card-section>
+
+      <q-separator dark inset />
+
+      <q-card-section>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  setup () {
+    return {
+      right: ref(false),
+    }
+  }
+})
+
+</script>
+
+<style>
+.inputcard {
+  width: 30%;
+}
+</style>
